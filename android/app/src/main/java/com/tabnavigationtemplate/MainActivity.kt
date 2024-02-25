621dd19f77f7d420d8d0cd7ba3ccf2eb2ee196da
Arnan Comia <arnancomia.trey@gmail.com>
2024-02-25 15:54:37 +0800

    Added app icon and change app name

--- android/app/src/main/java/com/tabnavigationtemplate/MainActivity.kt
+++ android/app/src/main/java/com/tabnavigationtemplate/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "TabNavigationTemplate"
+  override fun getMainComponentName(): String = "Tab Atlas"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
